ecb860d0d4438023a02fe287927152103c597bac
Gouranga-GH <post.gourang@gmail.com>
2025-07-09 11:36:59 +0530

    Refactoring

--- app.py
+++ app.py
@@ -174,4 +174,4 @@ with gr.Blocks(theme=gr.themes.Soft(primary_hue="green", secondary_hue="green"))
     """)
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=7860)
